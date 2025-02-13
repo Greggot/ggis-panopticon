f2f03fc82191d930808391a181e06dfec6142187
Sergey Vorontsov <aurum1997@gmail.com>
2025-02-13 17:35:36 +0300

    Поддержка `#` для простого формата тасок

--- tasks_parser/simple.py
+++ tasks_parser/simple.py
@@ -1,15 +1,18 @@
 from card_creator import Card_creator
 from card_creator_config import Card_creator_config
-from card_utils import card_from_types
+from card_utils import card_from_types, card_from_id
 from dev_tasks import parse_tasks_file
 from kaiten.card import CardType
 from kaiten.session import Session
 
 def create_cards_from_text_file_features(session: Session, path: str, config: Card_creator_config) -> None:
     for tasklist in parse_tasks_file(path):
-        story = card_from_types(session=session, type_ids={CardType.User_story, CardType.Enabler, CardType.Techdolg}, identificator=tasklist.story)
+        if tasklist.custom_id is None:
+            story = card_from_types(session=session, type_ids={CardType.User_story, CardType.Enabler, CardType.Techdolg}, identificator=tasklist.story)
+        else:
+            story = card_from_id(session, tasklist.custom_id)
         if story is None:
-            print(f'[WARNING] Не удалось отыскать карточку с номером {tasklist.story}')
+            print(f'[WARNING] Не удалось отыскать карточку с номером {tasklist.story}#{tasklist.custom_id}')
             continue
         if story.is_late:
             print(f'[WARNING] Истек срок карточки: {story.title}, deadline: {story.deadline}')
@@ -19,9 +22,12 @@ def create_cards_from_text_file_features(session: Session, path: str, config: Ca
 
 def create_cards_from_text_file_bugs(session: Session, path: str, config: Card_creator_config) -> None:
     for tasklist in parse_tasks_file(path):
-        bug = card_from_types(session=session, type_ids={CardType.Bug}, identificator=tasklist.story)
+        if tasklist.custom_id is None:
+            bug = card_from_types(session=session, type_ids={CardType.Bug}, identificator=tasklist.story)
+        else:
+            bug = card_from_id(session, tasklist.custom_id)
         if bug is None:
-            print(f'[WARNING] Не удалось отыскать карточку с номером {tasklist.story}')
+            print(f'[WARNING] Не удалось отыскать карточку с номером {tasklist.story}#{tasklist.custom_id}')
             continue
         if bug.is_late:
             print(f'[WARNING] Истек срок карточки: {bug.title}, deadline: {bug.deadline}')
