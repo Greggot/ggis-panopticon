8bdc13c5872795e80fa1e460bd9d456fd73075e8
Greggot <alexgerggot@gmail.com>
2025-03-13 01:02:54 +0300

    Add skird, ggis id test

--- c-dolphin/src/main.c
+++ c-dolphin/src/main.c
@@ -60,7 +60,7 @@ Card* request_current_card(const Env* env, const Card_request* request)
     return result;
 }
 
-void skird(const Env* env, const Dev_task_list* dev)
+void skird(const Env* env, const User* user, const Dev_task_list* dev, Create_paramters* parameters)
 {
     Story_list* story_ptr = dev->head_story;
     Card_request parent_request = {
@@ -75,78 +75,128 @@ void skird(const Env* env, const Dev_task_list* dev)
         print_string_view(&story_ptr->story.parent_story);
         printf("...\n");
 
-        Card* parent_card = NULL;
+        parameters->parent = NULL;
 
         printf("[] USER_STORY\n");
         parent_request.type_id = USER_STORY;
-        parent_card = request_current_card(env, &parent_request);
+        parameters->parent = request_current_card(env, &parent_request);
 
-        if (parent_card == NULL) {
+        if (parameters->parent == NULL) {
             printf("[] ENABLER\n");
             parent_request.type_id = ENABLER;
-            parent_card = request_current_card(env, &parent_request);
+            parameters->parent = request_current_card(env, &parent_request);
         }
 
-        if (parent_card == NULL) {
+        if (parameters->parent == NULL) {
             printf("[] BUG\n");
             parent_request.type_id = BUG;
-            parent_card = request_current_card(env, &parent_request);
+            parameters->parent = request_current_card(env, &parent_request);
         }
 
-        if (parent_card == NULL) {
+        if (parameters->parent == NULL) {
             printf("[] TECHDEBT\n");
             parent_request.type_id = TECHDEBT;
-            parent_card = request_current_card(env, &parent_request);
+            parameters->parent = request_current_card(env, &parent_request);
         }
 
-        if (parent_card == NULL)
-            printf("[ ] Search FAILED\n\n");
-        else {
-            printf("[X] Search finished\n\n");
-            deallocate_card(parent_card);
+        if (parameters->parent == NULL)
+            printf("[X] Search finished\n");
+
+        Task_list* task_ptr = story_ptr->story.tasks_head;
+        while (task_ptr != NULL) {
+            parameters->title = task_ptr->task;
+            create_card(env, user, parameters);
+            task_ptr = task_ptr->next;
         }
+
+        if (parameters->parent == NULL)
+            printf("[ ] Search FAILED\n");
+        else
+            deallocate_card(parameters->parent);
+        printf("\n");
         story_ptr = story_ptr->next;
     }
 }
 
+/// @brief По моит эмпирическим догадкам все виды карточек содержат
+/// бред в начале, после которого ставится @b точка, после которой
+/// идет месиво из цифр-букв, других точек до тех пор, пока не появится
+/// @b пробел. Не знаю, почему пробел, но я нашел его во всех карточках,
+/// которые мне было не лень просмотреть (где-то штук 20)
+String_view ggis_id_from_title(String_view title)
+{
+    String_view string_view = {
+        .ptr = title.ptr,
+        .size = 0
+    };
+
+    while (*string_view.ptr != '.')
+        ++string_view.ptr;
+    ++string_view.ptr;
+    const char* ptr = string_view.ptr;
+    while (*ptr != ' ' && *ptr != 0) {
+        ++ptr;
+        ++string_view.size;
+    }
+    if (*ptr != 0)
+        --string_view.size;
+    return string_view;
+}
+
+#if GGIS_ID_TEST
+#include <ctype.h>
+void test_ggis_id_case(const char* text)
+{
+    String_view input = create_string_view(text);
+    String_view result = ggis_id_from_title(input);
+
+    printf("[");
+    print_string_view(&result);
+    printf("]\n");
+}
+
+void test_ggis_id(void)
+{
+    test_ggis_id_case("[CAD]:EN.98.1. Проработать концепцию работы инструмента");
+    test_ggis_id_case("[CAD]:BUG.110.44746. Не выполняется операция");
+    test_ggis_id_case("[CAD]:TS.76.36867.36935. Нет объема у некоторых ПМ после импорта на ");
+    test_ggis_id_case("[CAD]:BUG.56e.22.45353. СЦЕНАРИЙ 5.3. Отсут");
+    test_ggis_id_case("[CAD]:EN.40e.1. Созданий копий исходных полигона");
+    test_ggis_id_case("[CAD]:F.113. Модель поиска");
+    test_ggis_id_case("[CAD]:DB.x.45908. Есть возможность экспортировать в");
+    test_ggis_id_case("[CAD]:DB.x.4590");
+    test_ggis_id_case("DB..4590");
+    test_ggis_id_case("[CAD]:TS.E.47-4.41617. Отрефакторить получение данных");
+    test_ggis_id_case("[MGM]:TS.1.4.44460. fe-common-audit. Добавить поле Количество записей в выгрузке csv");
+}
+#endif
+
 int main(void)
 {
+#if GGIS_ID_TEST
+    test_ggis_id();
+#endif
+
     Env env = read_env("../env/env.json");
-    printf("Accessing host: %s\n", env.kaiten_host.ptr);
-
-    Skird_config skird_config = read_skird_config("../env/skird_config/delivery.json");
-    printf("Config: %s\n", skird_config.size_text.ptr);
-
     Dev_task_list dev = parse_task_list("data.txt");
+    Skird_config skird_config = read_skird_config("../env/skird_config/delivery.json");
 
     requests_init();
-
-    String tags[2] = { create_string("ГГИС"), create_string("C++") };
-
     User user = request_current_user(&env);
-    Card parent_card = {
-        .id = 46276,
-        .sprint = 53,
-        .title = create_string("[CAD]:EN.131.13. Реализовать работу с компонентами связанности меша")
-    };
+
+    String_view tags[2] = { create_string_view("ГГИС"), create_string_view("C++") };
     Create_paramters create_parameters = {
         .title = create_string_view("Created by C"),
-        .parent = &parent_card,
+        .parent = NULL,
         .config = &skird_config,
         .tags_ptr = tags,
         .tags_size = sizeof(tags) / sizeof(String)
     };
-    // create_card(&env, &user, &create_parameters);
-    skird(&env, &dev);
+    skird(&env, &user, &dev, &create_parameters);
 
     requests_deinit();
     clean_task_list(&dev);
     delete_skird_config(&skird_config);
     delete_env(&env);
     delete_user(&user);
-    delete_string(&parent_card.title);
-
-    for (size_t i = 0; i < create_parameters.tags_size; ++i) {
-        delete_string(&tags[i]);
-    }
 }
